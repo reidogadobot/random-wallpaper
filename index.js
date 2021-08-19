6a41f66e11d6406d181e49c56aecb27f8c65f6a4
reidogadobot <79222246+reidogadobot@users.noreply.github.com>
2021-08-19 19:09:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const wallpaper = require("./database/wallpapers.json");
+const PORT = process.env.PORT || 3000;
+
+function random() {
+   return wallpaper[Math.floor(Math.random() * wallpaper.length)]
+}
+
+
+app.get("/wallpaper", (req, res) => {
+   res.json({result: random()})
+})
+
+app.listen(PORT, () => {
+   console.log(PORT)
+})
